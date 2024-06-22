9868146e953fed8867106332a937686b751a9f26
gustavo <gustavobpasquali2@gmail.com>
2024-06-22 02:39:23 -0300

    socorro tomara q de certo

--- goTest.go
+++ goTest.go
@@ -1,14 +1,11 @@
 package goTest
 
 type Mensagem struct {
-	IdClient           int
-	NovaPosX, NovaPosY int
-	PosX, PosY         int
-	Anterior           rune
-	Comando            rune
+	IdClient int
+	Comando  rune
 }
 
 type Client struct {
-	Id         int
-	PosX, PosY int
+	Id       int
+	Anterior rune
 }
